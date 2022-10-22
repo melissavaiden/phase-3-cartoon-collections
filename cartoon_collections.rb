fb1b90a5f748e052f260d0603a23da74defb82a5
Melissa Vaiden <melissavaiden@gmail.com>
2022-10-22 08:01:05 -0400

    commit tree 5939344f4dd9802453578dc9809655d7279d4c5a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arry
+  arry.each_with_index {|d, i| puts "#{i+1}. #{d}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arry
+  arry.map do |c|
+    c.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls array
+  if array.length >= 4
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
